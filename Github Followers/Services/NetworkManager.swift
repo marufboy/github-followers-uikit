9f4aab18fbee2e98e2f4c564eae3d90ef5dc51ba
marufboy_ <afif23ixa@gmail.com>
2024-01-30 21:24:20 +0800

    feat: update service using Result and add GFErrorMessage

--- Github Followers/Services/NetworkManager.swift
+++ Github Followers/Services/NetworkManager.swift
@@ -13,26 +13,26 @@ class NetworkManager {
     
     private init() {}
     
-    func getFollowers(for username: String, page: Int, completion: @escaping ([FollowerModel]?, String?) -> Void){
+    func getFollowers(for username: String, page: Int, completion: @escaping (Result<[FollowerModel]?, GFErrorMessage>) -> Void){
         let endpoint = baseURL + "\(username)/followers?per_page=100&page=\(page)"
         
         guard let url = URL(string: endpoint) else {
-            completion(nil, "This username created invalid request.")
+            completion(.failure(.invalidUsername))
             return
         }
         
         let task = URLSession.shared.dataTask(with: url) { data, response, err in
             if let _ = err {
-                completion(nil, "Unable to complete your request")
+                completion(.failure(.unableToComplete))
             }
             
             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                completion(nil, "Invalid response from the server")
+                completion(.failure(.invalidResponse))
                 return
             }
             
             guard let data = data else {
-                completion(nil, "The data received from server not valid")
+                completion(.failure(.invalidData))
                 return
             }
             
@@ -40,9 +40,46 @@ class NetworkManager {
                 let decoder = JSONDecoder()
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
                 let followers = try decoder.decode([FollowerModel].self, from: data)
-                completion(followers, nil)
+                completion(.success(followers))
             } catch {
-                completion(nil, "The data received from server not valid")
+                completion(.failure(.invalidData))
+            }
+        }
+        
+        task.resume()
+    }
+    
+    func getUserInfo(for username: String, completion: @escaping (Result<[UserModel]?, GFErrorMessage>) -> Void) {
+        //TODO: base endpoint
+        let endpoint = baseURL + "\(username)"
+        
+        guard let url = URL(string: endpoint) else {
+            completion(.failure(.invalidUsername))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, err in
+            if let _ = err {
+                completion(.failure(.unableToComplete))
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completion(.failure(.invalidResponse))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.invalidData))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let user = try decoder.decode([UserModel].self, from: data)
+                completion(.success(user))
+            } catch {
+                completion(.failure(.invalidData))
             }
         }
         
---
+++ Github Followers/Utilities/GFErrorMessage.swift
@@ -0,0 +1,17 @@
+//
+//  GFErrorMessage.swift
+//  Github Followers
+//
+//  Created by Muhammad Afif Maruf on 30/01/24.
+//
+
+import Foundation
+
+enum GFErrorMessage: String, Error {
+    case invalidUsername    = "This username created an invalid request. Please try again"
+    case unableToComplete   = "Unable to complete your request. Please check your internet connection"
+    case invalidResponse    = "Invalid response from the server. Please try again!"
+    case invalidData        = "The data received from the server was invalid. Please try again."
+    case unableToFavorite   = "There was an error favoriting this user. Please try again."
+    case alreadyInFavorites = "You have already favorited this user. You must really like them!"
+}
